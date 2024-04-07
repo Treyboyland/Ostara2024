2b54475dae11d76e8e42b0e42e90394ccb23989e
Roosevelt Boyland III <treyboyland@aol.com>
2024-04-07 13:38:38 -0500

    Adding background change

---
+++ Assets/Scripts/Utility/Events/ActualEvents/GameEventSprite.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent-On", menuName = "Utility/Game Event/Sprite", order = 5)]
+public class GameEventSprite : GameEventGeneric<Sprite>
+{
+
+}
